109bbde7f81fc3d3d23b968d799bf996bbaf1ab6
Samuel Le Guelvouit <r@inbowunicornf.tw>
2012-02-04 00:17:58 +0100

    edit post

--- src/admin.php
+++ src/admin.php
@@ -4,7 +4,7 @@ function getTitle(){
 }
 
 function r(){
-	header('location:admin.php');
+	header('location:admin.php');exit();
 }
 
 function absPath(){
@@ -12,12 +12,24 @@ function absPath(){
 	return substr($a,0,strlen($a)-10);
 }
 
-function i($l,$n){
-	echo "<p>$l<input type='text' name='$n'/></p>";
+function i($l,$n,$v){
+	echo "<p>$l<input type='text' name='$n' value='$v'/></p>";
 }
 
 function q($q){
 	return mysql_query($q);
+}
+
+function u($x){
+	return (isset($x["u"])&&strlen($x["u"]))?$x["u"]:str_replace(' ','-',$x["t"]);
+}
+
+function e($s){
+	return str_replace('\'','\'\'',$s);
+}
+
+function f($q){
+	return mysql_fetch_array($q);
 }
 
 session_start();
@@ -27,65 +39,68 @@ $w=$_POST;
 $x=$_GET;
 $p=array();
 $d=dir('tpl');
-while($e=$d->read()){
+while($e=$d->read())
 	if($e!="."&&$e!="..")
 		q("insert into t values(NULL,'".$e."')");
-}
+$d=array();
+$d[0]=$d[1]=$d[2]=$d[3]=$d[4]="";
 $q=q("select * from t");
-while($r=mysql_fetch_array($q))
+while($r=f($q))
 	$p[]=$r;
 if(isset($_SESSION["a"])){
-	if(isset($w["t"])){
-		$u=(isset($w["u"])&&strlen($w["u"]))?$w["u"]:str_replace(' ','-',$w["t"]);
-		q("insert into p values(NULL,'".$w["c"]."',".$w["p"].",'".$u."','".$w["t"]."')");
+	if(isset($x["v"])){
+		q("update p set c='".e($x["c"])."',p=".$x["p"].",n='".e(u($x))."',t='".e($x["t"])."' where i=".$x["v"]);
 		r();
 	}
-	else{
-		if(isset($x["d"])){
-			q("delete from p where i=".$x["d"]);
-			r();
-		}
+	if(isset($x["t"])){
+		q("insert into p values(NULL,'".e($x["c"])."',".$x["p"].",'".e(u($x))."','".e($x["t"])."')");
+		r();
+	}
+	if(isset($x["d"])){
+		q("delete from p where i=".$x["d"]);
+		r();
+	}
+	if(isset($x["e"]))
+		$d=f(q("select t,n,p,c,i from p where i=".$x["e"])); /* all todo*/
 	
-		include 'header.php'; 
-		$q=q("select * from p");
-		while($r=mysql_fetch_array($q)){
-			echo "<p><a href='".absPath()."/$r[3]/'>$r[4]</a> <a href='?e=$r[0]'>edit</a> <a href='?d=$r[0]'>del</a></p>";
-		}
+	include 'header.php'; 
+	$q=q("select * from p");
+	while($r=f($q))
+		echo "<p><a href='".absPath()."/$r[3]/'>$r[4]</a> <a href='?e=$r[0]'>edit</a> <a href='?d=$r[0]'>del</a></p>"; 
 ?>
-		<form href="" method="post">
+		<form href="">
 <?php 
-			i("Titre*","t");
-			i("Url","u"); 
+			echo ($d[0]!=""?"<input type='hidden' name='v' value='$d[4]'/>":"");
+			i("Titre*","t",$d[0]);
+			i("Url","u",$d[1]); 
 ?>
 			<p>Template*
 				<select name="p">
-					<?php foreach($p as $t){ ?>
-						<option value="<?php echo $t[0]; ?>"><?php echo $t[1]; ?></option>
-					<?php } ?>
+					<?php foreach($p as $t) ?>
+						<option value="<?php echo $t[0].'" '.($t[0]==$d[2]?"selected":""); ?>><?php echo $t[1]; ?></option>
 				</select>
 			</p>
 			<p class="tContent">Contenu*</p>
-			<textarea name="c"></textarea>
+			<textarea name="c"><?php echo $d[3]; ?></textarea>
 			<input type="submit" value="Valider"/>
 			<div class="clear"></div>
 			</form>
-<?php }
+<?php 
 }
 else{
-	if(isset($w["i"])){
-		if(mysql_fetch_array(mysql_query("select * from u where u.n='".$w["i"]."' and u.p='".md5($w["p"])."'")))
+	if(isset($x["i"])){
+		if(mysql_fetch_array(mysql_query("select * from u where u.n='".$x["i"]."' and u.p='".md5($x["p"])."'")))
 			$_SESSION["a"]=1;
 		r();
 	}
-	else{
-		include 'header.php'; 
+	include 'header.php'; 
 ?>
-		<form class="login" href="" method="post">
-			<?php i("ID","i"); ?>
-			<p>Pwd<input type="password" name="p"/></p>
-			<input type="submit" value="Valider"/>
-			<div class="clear"></div>
-		</form>
-<?php }
+	<form class="login" href="">
+		<?php i("ID","i",""); ?>
+		<p>Pwd<input type="password" name="p"/></p>
+		<input type="submit" value="Valider"/>
+		<div class="clear"></div>
+	</form>
+<?php 
 }
 include 'footer.php';
